2d48bce79ef00a18cbf36434291b49ff2576eb40
vincmarc <61312654+vincmarc@users.noreply.github.com>
2020-12-06 14:41:54 +0100

    update compiler ok
    replaced bid_id with contractor address, added see_tender, fixed compiler errors

--- TenderingSmartContract_toModify.sol
+++ TenderingSmartContract_toModify.sol
@@ -1,4 +1,4 @@
-pragma solidity >0.4.0 <0.7.0;
+pragma solidity ^0.4.14;
 pragma experimental ABIEncoderV2;
 
 import "splitstring.sol";
@@ -13,34 +13,31 @@ contract TenderingSmartContract is ContractString {
     struct BiddingOffer {
         address contractor;
         bytes32 hashOffer;
-        uint256 bidId;
         string description;
         bool valid;
         string separator;
+        string[] NewDescription;
         //mapping each bid id to its final description (the original splitted description)
         //if we had description = '12####1283####è bello####'
         //now we will have [12,1283,è bello] 
-        mapping(uint => string[]) NewDescription;
     }
 
     struct Tender {
-
+        uint256 tender_id;
         string tenderName;
         string description;
-        uint256 tenderId;
         uint256 bidOpeningDate;
-        uint256 bidSubmissionClosingDateHash;
         uint256 bidSubmissionClosingDateData;
-        uint[] bidList; // array where to store the index of all the bid
-        mapping(uint => BiddingOffer) bids;
-        mapping(address => uint) bidIdFromAddress;
+        uint256 bidSubmissionClosingDateHash;
+        address[] bidList; // array where to store the index of all the bid
+        mapping(address => BiddingOffer) bids;
         address tenderingInstitution;
         address winningContractor;
         
     }
     
     mapping (uint => Tender) public tenders;
-    uint[] public tenderList; // list of tender keys so we can enumarate them
+    uint[] public tenderList; //list of tender keys so we can enumarate them
     uint public tenderKeys = 0;
 
 
@@ -75,14 +72,17 @@ contract TenderingSmartContract is ContractString {
     // fine 
 
     
-    function CreateTender(string memory _tenderName, string memory _description, uint256 _daysUntilClosingDateHash,
-    uint256 _daysUntilClosingDateData) public  onlyAllowed{
-        tenders[tenderKeys].tenderName = _tenderName;
-        tenders[tenderKeys].description = _description;
-        tenders[tenderKeys].bidOpeningDate = now;
-        tenders[tenderKeys].bidSubmissionClosingDateHash = now + (_daysUntilClosingDateHash * 1 days);
-        tenders[tenderKeys].bidSubmissionClosingDateData = now + (_daysUntilClosingDateData * 1 days);
-        emit message("Tender Depoyed", msg.sender);
+    function CreateTender(string memory _tenderName, string memory _description,uint256 _daysUntilClosingDateData, uint256 _daysUntilClosingDateHash) public  isOwner{
+        Tender storage c = tenders[tenderKeys];
+        c.tender_id = tenderKeys;
+        c.tenderName = _tenderName;
+        c.description = _description;
+        c.bidOpeningDate = now;
+        c.bidSubmissionClosingDateHash= now + (_daysUntilClosingDateHash* 1 days);
+        c.bidSubmissionClosingDateData = now + (_daysUntilClosingDateData * 1 days);
+        c.tenderingInstitution= msg.sender;
+        c.bidList= new address[](0);
+        emit message("Tender Deployed", msg.sender);
 
         tenderKeys ++;
 
@@ -98,15 +98,9 @@ contract TenderingSmartContract is ContractString {
 
     // this function allowed contractors to participate the tender by sumbitting the hash
     function placeBid (uint256 _tenderKey, bytes32 _hashOffer) public inTimeHash(_tenderKey) {
-
-        uint bidKey = tenders[_tenderKey].bidList.length;
-        tenders[_tenderKey].bidList.push(bidKey);
-        tenders[_tenderKey].bids[bidKey].contractor = msg.sender;
-        tenders[_tenderKey].bids[bidKey].hashOffer = _hashOffer;
-        
-        //assigning to the address of the contractor its bidkey
-        tenders[_tenderKey].bidIdFromAddress[msg.sender] = bidKey;
-
+        Tender storage c = tenders[_tenderKey];
+        c.bids[msg.sender] = BiddingOffer(msg.sender,_hashOffer,"",false,"", new string[](0));
+        c.bidList.push(msg.sender);
     }
     
     //The following function returns the bid_id of a certain address.
@@ -117,47 +111,42 @@ contract TenderingSmartContract is ContractString {
     //contract.functions.concludeBid(tender_id,bid_id,unencrypted_message,separator)
     
    
-    function returningBidIdAddress (uint256 _tenderKey) public view returns(uint) {
-        
-        uint bid_id = tenders[_tenderKey].bidIdFromAddress[msg.sender];
-        return(bid_id);
-    }
-    
+
     
     modifier inTimeData (uint256 _tenderKey) {
         require(
-        (now > tenders[_tenderKey].bidSubmissionClosingDateHash) && (now < tenders[_tenderKey].bidSubmissionClosingDateData),
+        (now >= tenders[_tenderKey].bidSubmissionClosingDateHash) && (now < tenders[_tenderKey].bidSubmissionClosingDateData),
         "The data has to be sent after the hash closing date and before the data closing date."
         );
         _;
     } 
 
     // after the deadline contractors can send the actual offer
-    function concludeBid(uint256 _tenderKey, uint32 _bidkey, string memory _description) public inTimeData(_tenderKey) {
+    function concludeBid(uint256 _tenderKey, string memory _description, string memory _separator) public inTimeData(_tenderKey) {
     
         
         //assert(tenders[_tenderKey].bidSubmissionClosingDateData > now); // I have commented this line because the modifier inTimeData checks for this
         
         //assert that the it is the bids of the contractor that it is trying to conclude the Bid
-        assert(tenders[_tenderKey].bids[_bidkey].contractor == msg.sender);
+        assert(tenders[_tenderKey].bids[msg.sender].contractor == msg.sender);
         // check that the hash correspond
-        assert (keccak256(abi.encodePacked(_description)) == tenders[_tenderKey].bids[_bidkey].hashOffer);
+        assert (keccak256(abi.encodePacked(_description)) == tenders[_tenderKey].bids[msg.sender].hashOffer);
         // finally conclude the bid by submitting the restriction
-        tenders[_tenderKey].bids[_bidkey].description = _description;
+        tenders[_tenderKey].bids[msg.sender].description = _description;
         //memorizing the separator used in each bid
-        tenders[_tenderKey].bids[_bidkey].separator = _separator;
-        tenders[_tenderKey].bids[_bidkey].valid = true;
+        tenders[_tenderKey].bids[msg.sender].separator = _separator;
+        tenders[_tenderKey].bids[msg.sender].valid = true;
 
     }
     
     //with this function each bid_id is assigned to a list of the elements presented in the original string-offer
     
     function SplitDescription(uint256 _tenderKey) public onlyAllowed {
-        for (uint i = 0; i < tenders[_tenderKey].bidList.length; i++){
-             string memory separatorToUse  = tenders[_tenderKey].bids[i].separator;
+        for (uint i=0; i<tenders[_tenderKey].bidList.length; i++){
+             string memory separatorToUse  = tenders[_tenderKey].bids[tenders[_tenderKey].bidList[i]].separator;
             //UPDATE DESCRIPTION
-            string memory descriptionAtTheMoment = tenders[_tenderKey].bids[i].description;
-            tenders[_tenderKey].bids[i].NewDescription[i] = SMT(descriptionAtTheMoment,separatorToUse);
+            string memory descriptionAtTheMoment = tenders[_tenderKey].bids[tenders[_tenderKey].bidList[i]].description;
+            tenders[_tenderKey].bids[tenders[_tenderKey].bidList[i]].NewDescription = SMT(descriptionAtTheMoment,separatorToUse);
         }
     }
     
@@ -168,11 +157,11 @@ contract TenderingSmartContract is ContractString {
     function getBidsByKey(uint256 _tenderId) public returns (address[] memory, string[] memory) {
 
 
-        for (uint i = 0; i < tenders[_tenderId].bidList.length; i++){
+        for (uint i=0; i<tenders[_tenderId].bidList.length; i++){
 
-            if (tenders[_tenderId].bids[i].valid == true){
-               _contractors.push(tenders[_tenderId].bids[i].contractor);
-               _descriptions.push(tenders[_tenderId].bids[i].description);
+            if (tenders[_tenderId].bids[tenders[_tenderId].bidList[i]].valid == true){
+               _contractors.push(tenders[_tenderId].bids[tenders[_tenderId].bidList[i]].contractor);
+               _descriptions.push(tenders[_tenderId].bids[tenders[_tenderId].bidList[i]].description);
             }
 
         }
@@ -181,7 +170,7 @@ contract TenderingSmartContract is ContractString {
 
     }
 
-    function assignWinningContractor(uint256 _tenderId, uint256 _bidId) public {
+    function assignWinningContractor(uint256 _tenderId, address _bidId) public {
 
         // assert that only the "owner" of the bid can assign the winner
         assert(tenders[_tenderId].tenderingInstitution == msg.sender);
@@ -195,6 +184,14 @@ contract TenderingSmartContract is ContractString {
         }
 
     }
+    
+    function see_tender(uint _tenderId) public returns (uint  tender_id, string memory tenderName,string memory description,
+    uint256 bidOpeningDate,uint256 bidSubmissionClosingDateData,address[] memory bidList, BiddingOffer memory){
+        BiddingOffer memory to_store= tenders[_tenderId].bids[tenders[_tenderId].bidList[0]];
+        return (tenders[_tenderId].tender_id,tenders[_tenderId].tenderName,tenders[_tenderId].description,
+        tenders[_tenderId].bidOpeningDate,tenders[_tenderId].bidSubmissionClosingDateData,tenders[_tenderId].bidList,to_store);
+    }
 
 
 }
+
