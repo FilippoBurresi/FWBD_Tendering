2f75a1356c6b442a517e04c6f3329f9e767c283a
bosanipietro <51325989+bosanipietro@users.noreply.github.com>
2020-12-15 11:22:07 +0100

    Update PA.sol

--- PA.sol
+++ PA.sol
@@ -5,11 +5,11 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
  /// @title A smart contract for controlling the accessibility 
  /// @notice With the following contract we create two different 'characters':
  /// - the Public Administration: the address which deploys the TenderingSmart Contract will be authomatically granted with the PA role,
- ///   hence it will be able to call all those functions with the onlyPA modifier. It will also be in charged of deciding which firms 
- ///   can partecipate into the tender with the addFirm function, and additionally it could allow any other user to play the PA role.
+ ///   hence it will be able to call all those functions with the onlyPA modifier. It will also be in charge of deciding which firms 
+ ///   can partecipate in the tender with the addFirm function, and additionally it could allow any other user to play the PA role.
  ///   Furthermore, it can revoke any permission previously granted either to a firm or another public administration. 
  ///   Note that all those users added as PA or firm can - at any time - renounce the assigned role.
- /// - the firm : an address granted with the firm-role is able to take part into a tender as bidder and, thus, it can call all those 
+ /// - the firm : an address granted with the firm-role is able to take part in a tender as bidder and, thus, it can call all those 
  ///   functions in the TenderingSmart Contract with the OnlyFirm modifier.
  
 contract PA is AccessControl {
